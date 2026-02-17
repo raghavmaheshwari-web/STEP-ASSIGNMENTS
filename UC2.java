07cf51f20f6d32dd36b512b23528032b01d96839
raghavmaheshwari-web <raghavmaheshwari183@gmail.com>
2026-02-17 10:42:34 +0530

    Update and rename README.md to UC2.java

---
+++ UC2.java
@@ -0,0 +1,14 @@
+# STEP-ASSIGNMENTS
+public class uc2 {
+    public static void main(String[] args) {
+
+        System.out.println("   ***     ******   ******      ******");
+        System.out.println(" **   **   *     *** *     ***  ***     ");
+        System.out.println("**     **  *     *** *     ***  ***     ");
+        System.out.println("**     **  ******    ******       ***   ");
+        System.out.println("**     **  *         *              *** ");
+        System.out.println(" **   **   *         *         **   *** ");
+        System.out.println("   ***     *         *           ****   ");
+
+    }
+}
